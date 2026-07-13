0549299b01e6842c1eaa593ac4ba36b7c8c8d74b
maylove-lov <samj09808@gmail.com>
2026-07-13 21:33:18 +0700

    Change WebView background color to cyan

--- android/app/src/main/java/com/mport/app/MainActivity.java
+++ android/app/src/main/java/com/mport/app/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends BridgeActivity {
         super.onCreate(savedInstanceState);
 
         if (getBridge() != null && getBridge().getWebView() != null) {
-            getBridge().getWebView().setBackgroundColor(Color.WHITE);
+            getBridge().getWebView().setBackgroundColor(Color.CYAN);
         }
     }
 }
